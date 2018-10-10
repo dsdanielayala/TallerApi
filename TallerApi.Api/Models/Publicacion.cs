1a68ba0bb8712aceb4beee7c2a600c2e10fe71cc
Daniel Ayala <djdanielayalads99@gmail.com>
2018-10-09 19:15:09 -0500

    Se agrego modelo y context

---
+++ TallerApi.Api/Models/Publicacion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TallerApi.Api.Models
+{
+    /*[Table("Publicacion")]*/
+    public class Publicacion
+    {
+        public int Id { get; set; }
+
+        /*[MaxLength(50)]
+        [Required]*/
+        public string Usuario { get; set; }
+
+        /*[MaxLength(50)]
+        [Required]*/
+        public string Descripcion { get; set; }
+
+        
+        public DateTime FechaPublicacion { get; set; }
+
+        /*[MaxLength(10)]
+        [Required]*/
+        public int MeGustas { get; set; }
+
+       /* [MaxLength(10)]
+        [Required]*/
+        public int MeDisgusta { get; set; }
+
+       /* [MaxLength(1)]
+        [Required]*/
+        public int VecesCompartido { get; set; }
+
+        public Boolean EsPrivada { get; set; }
+
+
+    }
+}
---
+++ TallerApi.Api/Models/PublicacionContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TallerApi.Api.Models
+{
+    public class PublicacionContext
+    {
+
+    }
+}
